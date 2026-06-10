1088534bd3b8a7d772dd858634f879d1af26bb53
ppandit12 <pawanpandit9834@gmail.com>
2026-06-11 00:35:21 +0530

    fix: change platform flag default/env to pawan to resolve undefined on UI

--- src/frontend/components/PlatformFlag/PlatformFlag.tsx
+++ src/frontend/components/PlatformFlag/PlatformFlag.tsx
@@ -5,7 +5,7 @@ import * as S from './PlatformFlag.styled';
 
 const { NEXT_PUBLIC_PLATFORM = 'local' } = typeof window !== 'undefined' ? window.ENV : {};
 
-const platform = NEXT_PUBLIC_PLATFORM;
+const platform = NEXT_PUBLIC_PLATFORM && NEXT_PUBLIC_PLATFORM !== 'undefined' ? NEXT_PUBLIC_PLATFORM : 'pawan';
 
 const PlatformFlag = () => {
   return (
