4af635bdc18553622bd3c2ea4f1d651f3fce213a
ppandit12 <pawanpandit9834@gmail.com>
2026-06-10 23:53:07 +0530

    fix: remove unused imports and state variables in Footer to fix compilation

--- src/frontend/components/Footer/Footer.tsx
+++ src/frontend/components/Footer/Footer.tsx
@@ -1,24 +1,12 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-import { useEffect, useState } from 'react';
 import * as S from './Footer.styled';
-import SessionGateway from '../../gateways/Session.gateway';
-import { CypressFields } from '../../utils/Cypress';
 import PlatformFlag from '../PlatformFlag';
 
-const { userId } = SessionGateway.getSession();
-
 const Footer = () => {
-  const [sessionId, setSessionId] = useState('');
-
-  useEffect(() => {
-    setSessionId(userId);
-  }, []);
-
   return (
     <S.Footer>
-
       <p>
         <a href="https://www.kuamar.shop/">kumar shop</a>
       </p>
